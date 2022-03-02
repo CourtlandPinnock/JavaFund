b6cd043175291db482b0d472f18d31ae4847a28e
Courtland Pinnock <courtlandpinnock@gmail.com>
2022-03-02 13:44:47 +0000

    fin

---
+++ multiply/javascript.js
@@ -0,0 +1,18 @@
+function multiply(){
+    let i = 0;
+    let a;
+    let b;
+    while(i === 0){
+    a = prompt("Enter first number","");
+    if (!isNaN(a)){
+            b = prompt("enter second number","");
+            (i = 4);
+        }
+        else {
+            alert("Enter a number");
+        }
+    }
+    let total = a * b;
+    alert(total);
+    return total;
+}
